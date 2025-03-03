9580ff143d969c15ce0f1e8d0af3c4a27ffaf48d
hsindpure <85298665+hsindpure@users.noreply.github.com>
2025-03-03 14:53:23 +0530

    Update Scriptlpad.js

--- Scriptlpad.js
+++ Scriptlpad.js
@@ -1,33 +1,47 @@
-// Add this to your QlikSense load script to make use of the Excel Uploader extension
-
-// Create variable for Excel file path if it doesn't exist
-LET vExcelFilePath = '';
-
-// Main load script section that will use the Excel file path from the extension
-//SECTION ACCESS;
-//SECTION APPLICATION;
-
-// Clear previous data
-TRACE Starting data reload from Excel file: $(vExcelFilePath);
-DROP TABLE IF EXISTS ExcelData;
-
-// Check if the Excel file path is set
-IF len('$(vExcelFilePath)') > 0 THEN
-    // Load data from Excel file
-    ExcelData:
-    LOAD 
-        *
-    FROM [lib://DataConnections/$(vExcelFilePath)]
-    (ooxml, embedded labels, table is Sheet1);
-    
-    TRACE Excel data loaded successfully;
-ELSE
-    // Load sample or default data if no Excel file is specified
-    TRACE No Excel file specified, loading default data;
-    
-    ExcelData:
-    LOAD * INLINE [
-        Field1, Field2, Field3
-        'Sample', 123, 456
-    ];
-END IF
+<div class="excel-uploader-extension">
+    <div class="container">
+        <h2>Excel Data Uploader</h2>
+        
+        <div class="upload-section">
+            <div class="file-input-container">
+                <input type="file" id="fileInput" accept=".xlsx,.xls" />
+                <label for="fileInput" class="file-input-label">
+                    <span ng-if="!fileName">Choose Excel File</span>
+                    <span ng-if="fileName">{{fileName}}</span>
+                </label>
+            </div>
+            
+            <div class="button-container">
+                <button class="store-button" ng-click="uploadFile()" ng-disabled="uploading || !fileName">
+                    <span ng-if="!uploading">Store File</span>
+                    <span ng-if="uploading">Storing...</span>
+                </button>
+                
+                <button class="reload-button" ng-click="reloadApp()" ng-disabled="reloading || !uploadSuccess">
+                    <span ng-if="!reloading">Reload Data</span>
+                    <span ng-if="reloading">Reloading...</span>
+                </button>
+            </div>
+        </div>
+        
+        <div class="status-section">
+            <!-- Upload Status -->
+            <div class="status-message success" ng-if="uploadSuccess">
+                File uploaded successfully!
+            </div>
+            
+            <div class="status-message error" ng-if="uploadError">
+                Error: {{errorMessage}}
+            </div>
+            
+            <!-- Reload Status -->
+            <div class="status-message success" ng-if="reloadSuccess">
+                Data reloaded successfully! Charts will update automatically.
+            </div>
+            
+            <div class="status-message error" ng-if="reloadError">
+                Reload Error: {{errorMessage}}
+            </div>
+        </div>
+    </div>
+</div>
